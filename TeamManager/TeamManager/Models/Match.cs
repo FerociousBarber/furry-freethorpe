e661f8dde904daa90191829c61f3539c0c331c70
FerociousBarber <chaosphere_1@hotmail.com>
2016-05-13 13:04:18 +0100

    First commit, basic UI, player model but no match model yet. EF to be built afterwards.

---
+++ TeamManager/TeamManager/Models/Match.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamManager.Models
+{
+    public class Match
+    {
+    }
+}
---
+++ TeamManager/TeamManager/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamManager.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string firstNames { get; set; }
+        public string surname { get; set; }
+        public DateTime DOB { get; set; }
+        public int goals { get; set; }
+        public int appearances { get; set; }
+        public int minutesPlayed { get; set; }
+        public string personalNotes { get; set; }
+        public string coachingNotes { get; set; }
+        public string contactEmail { get; set; }
+
+        public virtual ICollection<Match> Matches { get; set; }
+    }
+}
